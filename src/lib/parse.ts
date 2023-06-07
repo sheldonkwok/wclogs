d0076a685dbfe5f54d600fe901cd54d982c0bded
Sheldon Kwok <sheldon@secondspectrum.com>
2023-06-07 11:40:04 -0700

    fix: Fix timers

--- src/lib/parse.ts
+++ src/lib/parse.ts
@@ -14,6 +14,7 @@ export interface Parsed {
 
 const ROLES = ["tanks", "healers", "dps"] as const;
 type Role = (typeof ROLES)[number];
+type RPlayer = Map<number, { role: Role; name: string }>;
 
 export function parseReports(reports: Report[]): Parsed[] {
   const allReports = reports.flatMap((r) => {
@@ -36,14 +37,7 @@ export function parseReports(reports: Report[]): Parsed[] {
         time: formatTime(f.keystoneTime),
         owner: r.owner.name,
         date: r.startTime + f.startTime,
-        players: f.friendlyPlayers
-          .map((p) => {
-            const rp = rPlayers.get(p);
-            if (!rp) return { role: "dps" as const, name: "?" };
-            return rp;
-          })
-          .sort((a, b) => ROLES.indexOf(a.role) - ROLES.indexOf(b.role))
-          .map((p) => p.name),
+        players: findPlayers(rPlayers, f.friendlyPlayers),
         url: `https://www.warcraftlogs.com/reports/${r.code}#fight=${f.id}`,
       };
     });
@@ -52,19 +46,30 @@ export function parseReports(reports: Report[]): Parsed[] {
   return dedupReports(allReports);
 }
 
+function findPlayers(rPlayers: RPlayer, playerIds: number[]): string[] {
+  return playerIds
+    .map((p) => {
+      const rp = rPlayers.get(p);
+      if (!rp) return { role: "dps" as const, name: "?" };
+      return rp;
+    })
+    .sort((a, b) => ROLES.indexOf(a.role) - ROLES.indexOf(b.role))
+    .map((p) => p.name);
+}
+
 function formatTime(ms: number): string {
   return new Date(ms).toISOString().slice(14, 19);
 }
 
 const MIN = 60_000;
 const S2_TIMES = new Map([
-  ["Uldaman: Legacy of Tyr", 40 * MIN],
-  ["Neltharus", 35 * MIN],
-  ["Brackenhide Hollow", 36 * MIN],
-  ["Halls of Infusion", 38 * MIN],
+  ["Uldaman: Legacy of Tyr", 35 * MIN],
+  ["Neltharus", 33 * MIN],
+  ["Brackenhide Hollow", 35 * MIN],
+  ["Halls of Infusion", 35 * MIN],
   ["Vortex Pinnacle", 30 * MIN],
-  ["Freehold", 33 * MIN],
-  ["Underrot", 33 * MIN],
+  ["Freehold", 30 * MIN],
+  ["Underrot", 30 * MIN],
   ["Neltharion's Lair", 33 * MIN],
 ]);
 
