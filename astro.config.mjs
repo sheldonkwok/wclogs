dbf272b420624616490e4ee8b829cf43e089d62f
Sheldon Kwok <sheldon@secondspectrum.com>
2023-11-09 14:08:00 -0800

    feat: Enable speed insights

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,7 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   output: "server",
   adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
+    webAnalytics: { enabled: true, },
+    speedInsights: { enabled: true },
   }),
 });
