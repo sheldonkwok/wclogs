06c43fbc7d2495b33fae5681706081da142eca23
Sheldon Kwok <sheldon@secondspectrum.com>
2024-10-03 20:59:07 -0700

    feat: Remove auto db flush

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -4,6 +4,4 @@ const redisUrl = import.meta.env.REDIS_URL;
 const redisClient = redis.createClient({ url: redisUrl });
 await redisClient.connect();
 
-await redisClient.flushDb();
-
 export default redisClient;
