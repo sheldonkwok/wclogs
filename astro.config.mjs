8665e390b2d00132a250c3168e4d15a072f5967b
Sheldon Kwok <sheldon@secondspectrum.com>
2025-02-16 10:15:41 -0800

    feat: Remove isr

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
   adapter: vercel({
     webAnalytics: { enabled: true, },
     speedInsights: { enabled: true },
-    isr: { expiration: 60 }, 
+    // isr: { expiration: 60 }, 
     edgeMiddleware: true
   }),
   image: {
